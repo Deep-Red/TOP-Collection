5db60bdf39766fb11fc9495aacaba6ccb52d5b6c
Deep Red <0k5786@gmail.com>
2015-09-29 16:11:31 -0400

    Trying again to get this up on github. Basic html and css form. js seems to be interacting with page finally but always returns an answer of 0. Needed the <script> tag to get it to interact it seems. Possibly a problem with my js right now causing the issue.

---
+++ euler1.js
@@ -0,0 +1,18 @@
+var isMultipe = function(number){
+if ((number % 3 === 0) || (number % 5 === 0)) {
+    return number;
+}
+    else {
+        return 0;
+    }
+};
+
+var sumOfMultiples = function(){
+    var input = document.getElementById("problem_1_input");
+    var answer = document.getElementById("answer");
+    var sum = 0
+    for(var i = 1; i < input; i++){
+        sum += isMultiple(i);
+    }
+    answer.innerHTML = sum;
+};
