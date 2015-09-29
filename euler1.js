90216440b6e1e4d7d5787b6e56e1ac84e9c9b2d4
Deep Red <0k5786@gmail.com>
2015-09-29 16:47:45 -0400

    Finally getting it to work. needed the .value method on line 11. Doesn't seem to need it on line 12 though, need to try and figure out why one and not the other. Still need to check and make sure the math is right.

--- euler1.js
+++ euler1.js
@@ -1,4 +1,4 @@
-var isMultipe = function(number){
+var isMultiple = function(number){
 if ((number % 3 === 0) || (number % 5 === 0)) {
     return number;
 }
@@ -8,9 +8,11 @@ if ((number % 3 === 0) || (number % 5 === 0)) {
 };
 
 var sumOfMultiples = function(){
-    var input = document.getElementById("problem_1_input");
+    var input = document.getElementById("problem_1_input").value;
     var answer = document.getElementById("answer");
     var sum = 0
+    alert("Test!" + 4);
+    alert(input)
     for(var i = 1; i < input; i++){
         sum += isMultiple(i);
     }
