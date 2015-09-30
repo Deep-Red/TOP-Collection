aac44067b9ba528845a974622d57ac6cbe71e2d2
Deep Red <0k5786@gmail.com>
2015-09-29 21:29:43 -0400

    Getting proper output for numbers over 4 million. returns 0 for everything else.

--- euler2.js
+++ euler2.js
@@ -1,28 +1,28 @@
-var a = 1
-var b = 2
-
-var isEven = function(b){
-	if(b % 2 === 0){
-		return b;
+var fibUpTo = function(b){
+	var i1 = 1;
+	var i2 = 1;
+	var sumOfEvens = 0;
+	var temp = 0;
+	while (i1 <= b){
+		if(b % 2 === 0){
+		sumOfEvens += i1;
 	}
-	else{
-		return 0;
-	}
+	temp = i1;
+	i1 = i2;
+	i2 = temp + i2;
 }
+return sumOfEvens;
+	};
 
 var findFib = function(){
 	var input = document.getElementById("problem_2_input").value;
 	var answer2 = document.getElementById("answer2");
-	var c = 0
-	if(input <= 4000000){
-	for(a = 1; a < input; (a += b)){
-		a += isEven(a);
-
-
-	}
-	answer2.innerHTML = a;
+	if (input <= 4000000){
+	var c = fibUpTo(input);
+	answer2.innerHTML = c;
 }
+
 	else{
-	answer2.innerHTML = "Pick a Number less than 4000000!"
+	answer2.innerHTML = "Pick a Number less than 4000000!";
 	}
 };
