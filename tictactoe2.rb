a75f4b3830875f8fb453139882142e9f8e198887
Deep Red <0k5786@gmail.com>
2016-04-15 01:09:39 -0400

    Mark works now. Game ends after 9 turns. Working on getting the winner to be recognized.

--- tictactoe2.rb
+++ tictactoe2.rb
@@ -1,20 +1,23 @@
 class Board
 
+WINS = [[0,1,2],[3,4,5,],[6,7,8],[0,3,6],[2,5,8],[1,4,7],[0,4,8],[2,4,6]]
+
 def initialize()
-	@gamestate = Array.new("a","b","c","d","e","f","g","h","i")
-	end
+	@gamestate = Array.new(9, " ")
 end
 
 def horizontal_line()
-	puts "-+-+-"
+	dash = "\u2014"
+	dn = dash.encode('utf-8')
+	puts "#{dn}+#{dn}+#{dn}"
 end
 
 def show_gamestate()
-	puts @gamestate[0].join "|"
+	puts @gamestate[0..2].join "|"
 	horizontal_line
-	puts @gamestate[1].join "|"
+	puts @gamestate[3..5].join "|"
 	horizontal_line
-	puts @gamestate[2].join "|"
+	puts @gamestate[6..9].join "|"
 end
 
 def show_board_map()
@@ -25,20 +28,37 @@ def show_board_map()
 	puts "7|8|9"
 end
 
+def winner
+	
+end
 def mark(player, choice)
 	piece = player == 1 ? "X" : "O"
+	pick = (choice.to_i - 1)
+	@gamestate[pick] = [piece]
+end
+
+def game_over_check(player, turn, choice)
 	pick = choice.to_i
-	row = (pick.to_f / 3).ceil
-	col = (pick % 3) == 0 ? 3 : (pick % 3)
-	puts @gamestate[row[col]]
-	@gamestate[row[col]] = [piece]
- 
-	show_gamestate
+	if WINS.any? { |line| 
+		line.all? { |square| 
+			@gamestate[square] == "X" } }
+	then 
+		return true
+	elsif turn == 9
+		return true
+	else 
+		return false
+	end
+end
+
+def goodbye
+	puts "This game is over."
 end
 
 def play(turn)
+	game_over = 0
 	turn += 1
-	player = turn % 2 ? 1 : 2
+	player = (turn % 2) == 1 ? 1 : 2
 
 	puts "Turn #{turn}:" 
 	puts "It is player #{player}'s turn!"
@@ -47,6 +67,9 @@ def play(turn)
 	puts "Which space would you like to choose, Player #{player}?"
 	choice = gets.chomp
 	mark(player, choice)
+	show_gamestate
+	game_over_check(player, turn, choice) ? goodbye : play(turn)
+
 end
 
 end
@@ -54,6 +77,7 @@ end
 
 gameboard = Board.new
 turn = 0
+winner = 0
 puts "Welcome to Tic Tac Toe"
 puts "Use the following space numbers to choose your move."
 gameboard.show_board_map
