b5485dbff0e7da55a68cff7f34195e5dda4b768a
Deep Red <0k5786@gmail.com>
2016-04-27 12:44:38 -0400

    Rewrite from draft as mastermind2.rb.  Evaluates first guess against codekey.

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,7 @@
 class Gameboard
 	require 'colorize'
 	@@colormap = ["1".red, "2".yellow, "3".green, "4".cyan, "5".blue, "6".magenta]
+	@@colormap2 = ["\e[0;31;49mX\e[0m", "\e[0;33;49mX\e[0m", "\e[0;32;49mX\e[0m", "\e[0;36;49mX\e[0m", "\e[0;34;49mX\e[0m", "\e[0;35;49mX\e[0m"]
 
 
 def initialize()
@@ -11,29 +12,28 @@ end
 
 def show_gamestate()
 	puts @@colormap.join(" ")
+#	puts @@colormap.inspect
 	puts "\n X X X X \n".black.bold
+	puts "\n X X X X \n".black.bold.inspect
 	
 	
 end
 
-def check_cows(guess_no_b, check_arr, bull_arr, turn, codekey)
-	cow_arr = []
-	puts guess_no_b.inspect
-	puts check_arr.inspect
-	#puts codekey
-	#puts guess.inspect
-	#puts "you are here"
-	check_arr.map! { |element| 
-				guess_no_b.include?(element) ? element : nil }
-	puts check_arr.inspect
-	#puts cow_arr
-	#check_bulls(cow_arr, turn)
-end
+#def check_cows(guess_no_b, check_arr, bull_arr, turn, codekey)
+#	puts guess_no_b.inspect
+#	puts check_arr.inspect
+#	#puts codekey
+#	#puts guess.inspect
+#	#puts "you are here"
+#	check_arr.map! { |element| 
+#				guess_no_b.include?(element) ? element : nil }
+#	puts check_arr.inspect
+#	#puts #check_bulls(turn)
+#end
 
 def check_bulls(guess, turn)
 	codekey = @codekey
 	bull_arr = []
-	cow_arr = []
 	check_arr = guess
 	guess_no_b = []
 	guess.each_with_index { |g, i| 
@@ -45,14 +45,14 @@ def check_bulls(guess, turn)
 			check_arr[i] = nil
 		end
 	}
-	puts bull_arr.inspect
-	check_cows(guess_no_b, check_arr, bull_arr, turn, codekey)
-	generate_feedback(bull_arr, cow_arr, codekey)
+#	puts bull_arr.inspect
+#	check_cows(guess_no_b, check_arr, bull_arr, turn, codekey)
+	generate_feedback(bull_arr, codekey, guess)
 
 end
 
 def valid_guess?(guess, turn)
-	guess.length != 4 ? invalid_guess(guess, turn) :
+	guess.length != 4 ? invalid_guess(guess, turn) : 
 	check_bulls(guess, turn)
 end
 
@@ -62,15 +62,32 @@ def invalid_guess(guess, turn)
 	play(turn - 1)
 end
 
-def generate_feedback(bull_arr, cow_arr, codekey)
+def display_guess(guess)
+	dg = []
+	guess.each_with_index{|g, i|
+	dg[i] = @@colormap2[(g.to_i-1)]
+	}
+	puts " " + dg.join(" ")
+end
+
+def generate_feedback(bull_arr, codekey, guess)
 	feedback = codekey
-	eval = []
+	eval = ["|".red, "|".red, "|".red, "|".red]
 	bull_arr.each_with_index{|b, i| 
-		if b == "B" 
-			feedback[i] = "X".black
-		else feedback[i] = nil
+		if b == "B"
+			feedback[i] = @@colormap2[(i.to_i - 1)]
+			eval[i] = "|".green
+			guess[i] = "O"
+#			codekey[i] = "X"
 		end
 	}
+	guess.each_with_index{|g, i|
+		if g == feedback.any?
+			eval[i] = "|".yellow
+			end
+	}
+
+	
 	puts " " + feedback.join(" ") + (eval.join)
 end
 
@@ -81,6 +98,7 @@ def play(turn)
 	
 	puts "Guess Below:"
 	guess = gets.chomp.split("")
+	display_guess(guess)
 	guess.map!{ |x| x.to_i}
 	valid_guess?(guess, turn)
 
---
+++ mastermind2.rb
@@ -0,0 +1,81 @@
+class Gameboard
+	@@colormap = ["\e[0;31;49m1\e[0m", "\e[0;33;49m2\e[0m", "\e[0;32;49m3\e[0m", "\e[0;36;49m4\e[0m", "\e[0;34;49m5\e[0m", "\e[0;35;49m6\e[0m"]
+	@@colormap2 = ["\e[0;31;49mX\e[0m", "\e[0;33;49mX\e[0m", "\e[0;32;49mX\e[0m", "\e[0;36;49mX\e[0m", "\e[0;34;49mX\e[0m", "\e[0;35;49mX\e[0m"]
+
+def initialize()
+	@codekey = Array.new(4, 0)
+	@codekey.map!{rand(1..6)}
+	puts @codekey.inspect
+end
+
+def show_gamestate()
+	puts @@colormap.join(" ")
+	puts "\e[1;30;49m\n X X X X \n\e[0m"
+end
+
+def evaluate_guess(guess, turn)
+	feedback = @codekey
+	feedbacktemp = feedback
+	pegs = ["\e[0;31;49m|\e[0m", "\e[0;31;49m|\e[0m", "\e[0;31;49m|\e[0m", "\e[0;31;49m|\e[0m"]
+
+	feedback.each_with_index{|f, i|
+		if feedback[i] == guess[i]
+			pegs[i] = "\e[0;32;49m|\e[0m"
+			feedback[i] = "#{rand()}" #eliminates bulls from feedback
+#			guess[i] = "#{rand()}"
+		end
+	}
+
+	puts feedback.inspect + " " + pegs.join("")
+
+	guess.each_with_index{|f, i|
+		feedback.find_index{ |c|
+			if c == f
+			pegs[i] = "\e[0;33;49m|\e[0m"
+			feedback[c.to_i] = "#{rand()}"
+			feedback[i] = "#{rand()}"
+			feedbacktemp[i] = "#{rand()}"
+			puts feedback
+		end }
+		}
+
+	puts "#{display_guess(guess)}" + " " + pegs.join("")
+
+end
+
+def valid_guess?(guess, turn)
+	guess.length != 4 ? invalid_guess(guess, turn) : 
+	evaluate_guess(guess, turn)
+end
+
+def invalid_guess(guess, turn)
+	puts "That's not a valid guess!"
+	puts "Try again!"
+	play(turn - 1)
+end
+
+def display_guess(guess)
+	dg = []
+	guess.each_with_index{|g, i|
+	dg[i] = @@colormap2[(g.to_i-1)]
+	}
+	print " " + dg.join(" ")
+end
+
+
+def play(turn)
+	turn += 1
+	puts "Turn #{turn}:"
+	show_gamestate
+	puts "Guess Below:"
+	guess = gets.chomp.split("")
+	display_guess(guess)
+	puts ""
+	guess.map!{ |x| x.to_i}
+	valid_guess?(guess, turn)
+end
+
+end
+
+x = Gameboard.new
+x.play(0)
