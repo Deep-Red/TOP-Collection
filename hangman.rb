f0335f57707d52542bf27afa135bf9e4b0edbdb6
Deep Red <0k5786@gmail.com>
2016-05-24 18:53:32 -0400

    choose_word method picks a word to serve as the key

--- hangman.rb
+++ hangman.rb
@@ -1,21 +1,25 @@
 class Game
 
 def initialize
+	possible_words = []
 	dictionary = "5desk.txt"
 	wordcount = 0
 	File.open(dictionary, "r") do |file|
 		file.readlines.each_with_index do |line, idx|
 		wordlength = line.chomp.length
-		if wordlength > 3 && wordlength < 13
-			puts "#{line.chomp} #{idx} #{wordcount} #{wordlength}"
+		if wordlength > 4 && wordlength < 13
+			#puts "#{line.chomp} #{idx} #{wordcount} #{wordlength}"
+			possible_words.push(line.chomp)
 			wordcount += 1
 		end
 	end
+	choose_word(possible_words, wordcount)
 end
 end
 
-def choose_word
-
+def choose_word(possible_words, wordcount)
+	word_num = rand(wordcount-1)
+	@word = possible_words[word_num]
 end
 
 def player_guess
