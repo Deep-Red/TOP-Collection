ab39be2f0921fccc6c9e861581b69a1499752486
Deep Red <0k5786@gmail.com>
2016-05-24 19:15:23 -0400

    defined the show_gamestate method and evaluate_guess meghod.

--- hangman.rb
+++ hangman.rb
@@ -14,31 +14,43 @@ def initialize
 		end
 	end
 	choose_word(possible_words, wordcount)
+	incorrect_guesses = []
+	knowledge = [wordcount, "_"]
+	play(0, knowledge, incorrect_guesses)
 end
 end
 
 def choose_word(possible_words, wordcount)
 	word_num = rand(wordcount-1)
-	@word = possible_words[word_num]
+	@word = possible_words[word_num].downcase
 end
 
 def player_guess
-
+	puts "What letter would you like to guess?"
+	guess = gets.downcase.chomp
+	evaluate_guess(guess)
 end
 
 def evaluate_guess
-
+	guess =~ /[[:alpha:]]/ ? true : puts "Invalid guess!" play(turn-1, knowledge, incorrect_guesses) 
 end
 
-def display_gamestate
-
+def show_gamestate(misses, knowledge, incorrect_guesses)
+	puts Array.new(misses, "\e[0;31;49mX\e[0m ")
+	puts knowledge
+	puts incorrect_guesses
 end
 
 def gameover?
 
 end
 
+def play(turn, knowledge, incorrect_guesses)
+	turn += 1
+	show_gamestate(misses, knowledge, incorrect_guesses)
+	player_guess
 
+end
 
 Game.new
 
