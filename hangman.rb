0ce4d73e57d742b9214f084df67384078f32e34a
Deep Red <0k5786@gmail.com>
2016-05-24 18:33:05 -0400

    restructured as a class with an initialize method.

--- hangman.rb
+++ hangman.rb
@@ -1,7 +1,19 @@
-dictionary = "5desk.txt"
+class Game
 
-File.open(dictionary, "r") do |file|
-	file.readlines.each_with_index do |line, idx|
-		puts "#{line.chomp} #{idx} #{line.length}"
+def initialize
+	dictionary = "5desk.txt"
+	wordcount = 0
+	File.open(dictionary, "r") do |file|
+		file.readlines.each_with_index do |line, idx|
+		wordlength = line.chomp.length
+		if wordlength > 3 && wordlength < 13
+			puts "#{line.chomp} #{idx} #{wordcount} #{wordlength}"
+			wordcount += 1
+		end
 	end
+end
+end
+
+Game.new
+
 end
