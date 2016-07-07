9d9a21486c672382b80080bb8ee767f9f8e3fb7b
Deep Red <0k5786@gmail.com>
2016-07-06 21:14:38 -0400

    About to revamp the entire approach to this problem. The program currently runs with no errors, but builds two unsorted arrays and doesn't return either of them.

--- rubymisc/merge_sort.rb
+++ rubymisc/merge_sort.rb
@@ -1,4 +1,18 @@
-def merge_sort(arr)
-	if arr.length < 3
-		arr[0] <=> arr[-1]
-		
+def merge_sort(arr, result = [])
+	return result = [arr[0] <=> arr[-1]] if arr.length <= 2
+	arr_1 = arr[0..(arr.length/2)]
+	arr_2 = arr[((arr.length/2)+1)..-1]
+	merge_sort(arr_1, result)
+	merge_sort(arr_2, result)
+	puts arr_1.inspect
+	puts arr_2.inspect
+	puts "."
+	if arr_1[0] <=> arr_2[0]
+		result = [arr_2[0], arr_1[0]]
+	else
+		result = [arr_1[0], arr_2[0]]
+	end
+	return result if result.length == arr.length
+end
+
+puts merge_sort([1, 5, 2, 4, 7, 88, 3, 15])
