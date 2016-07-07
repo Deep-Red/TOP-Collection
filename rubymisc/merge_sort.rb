cfa90cc2884a514ed56588850b83bf3b2bf47bde
Deep Red <0k5786@gmail.com>
2016-07-07 00:01:34 -0400

    Changing tact again.

--- rubymisc/merge_sort.rb
+++ rubymisc/merge_sort.rb
@@ -1,18 +1,14 @@
 def merge_sort(arr, result = [])
-	return result = [arr[0] <=> arr[-1]] if arr.length <= 2
+	return if arr.length <= 1
 	arr_1 = arr[0..(arr.length/2)]
 	arr_2 = arr[((arr.length/2)+1)..-1]
-	merge_sort(arr_1, result)
-	merge_sort(arr_2, result)
 	puts arr_1.inspect
 	puts arr_2.inspect
+	puts result.inspect
 	puts "."
-	if arr_1[0] <=> arr_2[0]
-		result = [arr_2[0], arr_1[0]]
-	else
-		result = [arr_1[0], arr_2[0]]
-	end
+	
+	result << arr_1.each.find { |a| arr_2.include? { |b| b < a }
 	return result if result.length == arr.length
 end
 
-puts merge_sort([1, 5, 2, 4, 7, 88, 3, 15])
+puts merge_sort([5, 1, 2, 4, 7, 88, 3, 15])
