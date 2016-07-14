2440e1af836e1a95414ff8b32d7a908108829c05
Deep Red <0k5786@gmail.com>
2016-07-13 20:38:17 -0400

    #build_tree seems to be creating a technically correct, but inelegant and inefficient structure.

--- rubymisc/binary_tree.rb
+++ rubymisc/binary_tree.rb
@@ -2,11 +2,11 @@ class Node
 
 	attr_accessor :left, :right, :parent, :value
 
-	def initialize(value, parent = nil)
+	def initialize(value, parent = nil, l_child = nil, r_child = nil)
 		@value = value
 		@parent = parent
-		@left = nil
-		@right = nil
+		@left = l_child
+		@right = r_child
 	end
 
 end
@@ -30,20 +30,26 @@ class Tree
 #		puts last.value
 		array.each do |v|
 			set = nil
-			puts last
-			if last == nil || v > last.value 
+			puts last.value
+			if v > last.value 
 				if last.right == nil
 					last.right = Node.new(v, last)
+					last = last.right
 					set = true
+					puts "one"
 				else
 					last = last.right
+					puts "two"
 				end
 			else
 				if last.left == nil
 					last.left = Node.new(v, last)
+					last = last.left
 					set = true
+					puts "three"
 				else
 					last = last.left
+					puts "four"
 				end
 			end
 		end
@@ -74,15 +80,16 @@ class Tree
 	end
 
 	def get_sorted_tree(node = @root, result = [])
-#		result << @root.value
+		result << "#{node.value}: #{node.left.value if node.left != nil}: #{node.right.value if node.right != nil}!"
+
 		get_sorted_tree(node.left, result) if node.left
-		result << node.value
 		get_sorted_tree(node.right, result) if node.right
 		return result
 	end
 
 end
 
-myarray = [2, 3, 6, 8, 99, 7, 5, 33, 21, 1, 3]
-mytree = Tree.new(myarray)
-puts mytree.get_sorted_tree.inspect
+my_array = Array(2..15)
+puts my_array.inspect
+my_tree = Tree.new(my_array)
+puts my_tree.get_sorted_tree.inspect
