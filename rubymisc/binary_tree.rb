0e771606662f4dbacfb73666f7b6fbd917228be7
Deep Red <0k5786@gmail.com>
2016-07-27 11:59:33 -0400

    add_node method rewrittten as case instead of ITE.

--- rubymisc/binary_tree.rb
+++ rubymisc/binary_tree.rb
@@ -1,12 +1,13 @@
 class Node
 
-attr_accessor :left, :right, :parent, :value
+attr_accessor :left, :right, :parent, :value, :count
 
 	def initialize(value, parent = nil, l_child = nil, r_child = nil)
 		@value = value
 		@parent = parent
 		@left = l_child
 		@right = r_child
+		@count = 1
 		puts "#{@value} added as #{self}" #Confirms a node was added to see when exactly the code breaks down.
 	end
 
@@ -23,13 +24,10 @@ def initialize(array)
 end
 
 def add_node(new_node, position = @root)
-	x = position.value
-#	puts x
-#	puts new_node
-	if new_node < x
-		position.left.nil? ? position.left = Node.new(new_node) : add_node(new_node, position.left)
-	else
-		position.right.nil? ? position.right = Node.new(new_node) : add_node(new_node, position.right)
+	case new_node <=> position.value
+	when 0 then position.count += 1
+	when -1	then position.left.nil? ? position.left = Node.new(new_node) : add_node(new_node, position.left)
+	when 1 then	position.right.nil? ? position.right = Node.new(new_node) : add_node(new_node, position.right)
 	end
 end
 
@@ -56,7 +54,7 @@ def dfs_rec(target)
 end
 
 def get_sorted_tree(node = @root, result = [])
-	result << "#{node.value}: #{node.left.value if node.left != nil}: #{node.right.value if node.right != nil}!"
+	result << "#{node.value} #{node.count if node.count > 1}: #{node.left.value if node.left != nil}: #{node.right.value if node.right != nil}!"
 	get_sorted_tree(node.left, result) if node.left
 	get_sorted_tree(node.right, result) if node.right
 	return result
@@ -65,7 +63,7 @@ end
 end
 
 
-my_array = [34, 11, 23, 43, 87, 5, 1, 27, 44, 34, 2, 6, 88]
+my_array = [34, 11, 11, 23, 43, 87, 5, 1, 27, 44, 34, 2, 6, 88, 22, 22, 33, 33, 22, 54, 45, 45, 45, 77, 44, 55, 74, 22, 33, 1, 5, 7, 43]
 #my_array = Array(2..15)
 puts my_array.inspect
 my_tree = Tree.new(my_array)
