f83a7305ef421e7bb45b44f35ad2e96ffc7c21e8
Deep Red <0k5786@gmail.com>
2016-07-30 17:34:51 -0400

    #potential_squares working, focusing on #valid_square? now

--- rubymisc/knight_moves.rb
+++ rubymisc/knight_moves.rb
@@ -6,21 +6,36 @@ class Board
 end
 
 class Knight
+	attr_accessor :position, :paths
+
 	def initialize(position = [7,1])
 		@position = position
+		@paths = [[1,2],[2,1],[-1,2],[-2,1],[1,-2],[2,-1],[-1,-2],[-2,-1]]
 	end
 
 	def move
 		@position 
 
+
 	end
 
-	def potential_suares(@position)
+	def potential_squares()
+		moves = []
+		@paths.each_with_index { |x, i| moves[i] = [x[0] + @position[0], x[1] + @position[1]] }
 
+		puts moves.inspect
+		moves = moves.each { |x| valid_square?(x) }
+		puts moves.inspect
 	end
 
 	def valid_square?(target)
-		target.each{ |x| (0..7).include?(x)} ? return true : return false
+		puts target.inspect
+		target.each{ |x| x < 8 && x > -1 }
+			puts "hi"
+			return target 
+		else
+			return nil
+		end
 	end
 
 	def find_path(position = @position, target)
@@ -29,5 +44,8 @@ class Knight
 end
 
 
-my_board =Board.new(8)
-
+my_board = Board.new(8)
+my_knight = Knight.new([0,0])
+puts my_knight.position.inspect
+puts my_knight.paths.inspect
+my_knight.potential_squares
