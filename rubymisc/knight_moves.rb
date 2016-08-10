373fd091d3662199c57bd67a83e947ebdd0a25a8
Deep Red <0k5786@gmail.com>
2016-08-10 19:27:37 -0400

    Erased earlier draft of knight_moves.rb. Renamed knight_moves2.rb to knight_moves.rb. Deleted marker comments from knight_moves.rb.

--- rubymisc/knight_moves.rb
+++ rubymisc/knight_moves.rb
@@ -1,46 +1,34 @@
 class Node
 	attr_accessor :coord, :parent, :children
 
-	def initialize(coord, parent = nil, children = nil)
+	def initialize(coord, parent = nil)
 		@coord = coord
 		@parent = parent
-		@children = children		
+		@children = nil
 	end
 end
 
 class Knight
-	attr_accessor :position, :paths, :target, :legal_routes
+	attr_accessor :position, :target, :legal_routes
 
 	def initialize(a = [7,1])
 		@position = Node.new(a)
 		@legal_routes = [[1,2],[2,1],[-1,2],[-2,1],[1,-2],[2,-1],[-1,-2],[-2,-1]]
-		@paths
 	end
 
-	def move
-		@position 
-
-
-	end
-
-	def potential_squares(current = @position.coord)
-		current = current
-		puts "YO #{current}"
+	def potential_squares(from = @position)
+		from = from
+		fromc = from.coord
 		moves = []
 		temp = []
-		@legal_routes.each_with_index { |x, i| temp[i] = [x[0] + current[0], x[1] + current[1]] }
-		temp.each do |x| 
-			moves << x if valid_square?(x)
-		end
-#		puts moves.inspect
-		moves.each{ |ns| Node.new(ns, current)}
-		position.children = moves
+		@legal_routes.each_with_index{ |x, i| temp[i] = [x[0] + fromc[0], x[1] + fromc[1]] }
+		temp.each { |x| moves << Node.new(x, from) if valid_square?(x) }
 		return moves
 	end
 
 	def valid_square?(target)
 		a = target[0]
-		b = target[1] 
+		b = target[1]
 		if a < 8 && a > -1 
 			b < 8 && b > -1 ? true : false
 		else
@@ -48,66 +36,44 @@ class Knight
 		end
 	end
 
-#	def find_path(target, current = @position)
-#		current = current
-#		target = target
-#		queue = [current]
-#		visited = [current]
-#		path = []
-#		puts "Tracing path from #{current.inspect} to #{target.inspect}"
-#
-#
-#		while queue != []
-#			if current == target
-#				path << current.unshift
-#				return path
-#			elsif current != nil
-#				visited << current
-##				puts "current is #{current}"
-#				self.potential_squares(current).each{ |x| queue << x }
-#				current = queue.shift
-#			else 
-#				current = queue.shift
-#			end
-#		end
-#	end
-
-#	def find_path(target, start = @position)
-#		position = start
-#		queue = position
-#		target = target
-#
-#		queue.unshift(position)
-#
-#		until position == target
-#			self.potential_squares(current).each{ |x| queue << x }
-#			position == queue.unshift
-#		end
-#	end
-
-	def find_path(to, from = @coord)
+	def find_path(to)
 		target = to
-		current = from
-		queue = from
-		visited = from
-
+		current = @position
+		queue = []
+		visited = []
+		queue << current
+		visited << current.coord
 		while queue != []
-			return current if current == target
-			queue << potential_squares(current)
-			puts queue
+			if current.coord == target
+				display_path(current) 
+				exit
+			else
+			potential_squares(current).each do |x|
+				visited.include?(x.coord) ? next : queue << x
+			end
+			visited << current.coord
+			current = queue.shift
+
+			end
 		end
-
-
-
 	end
+
+	def display_path(dest)
+		path = [dest]
+		current = dest
+		while current != @position
+			current = current.parent
+			path.unshift current
+		end
+		puts "The following moves will get your knight from its current position (#{@position.coord.inspect}) to the target square (#{dest.coord.inspect})."
+		path.each {|x| puts x.coord.inspect }
+	end
+
 
 end
 
+my_knight = Knight.new([1, 2])
+puts my_knight.find_path([0, 7])
 
-my_knight = Knight.new([3, 3])
-#puts my_knight.find_path([7,1])
 
-puts my_knight.legal_routes.inspect
-puts my_knight.potential_squares.inspect
-my_knight.find_path([3, 2])
 
