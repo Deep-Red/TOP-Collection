3113e03d34a8fe3a24fb0295e030d40b1c0d0574
Deep Red <0k5786@gmail.com>
2016-08-27 05:43:58 -0400

    connect_four_spec.rb begun

---
+++ rubymisc/connect_four/spec/connect_four_spec.rb
@@ -0,0 +1,23 @@
+require "connect_four"
+
+include ConnectFour
+
+describe Player do
+	
+end
+	
+describe Board do
+
+end
+
+context "when player 1 has won" do
+	it "returns true for player 1 as the winner and false for player 2"
+	end
+end
+
+context "when player 2 has won" do
+	it "returns true for player 2 as the winner and false for player 1"
+	end
+end
+
+end
