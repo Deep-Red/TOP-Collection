3bfc2425df8d6be43ebdb1cc85542d92002aa6a6
Deep Red <0k5786@gmail.com>
2016-09-02 16:14:57 -0400

    connect_four tdd completed, with extensive assistance

--- rubymisc/connect_four/lib/connect_four.rb
+++ rubymisc/connect_four/lib/connect_four.rb
@@ -1,3 +1,161 @@
-class ConnectFour
+class Board
+  attr_reader :grid, :game_status
 
+  def initialize
+    @height = 6
+    @width = 7
+    @grid = Array.new(@width) {Array (@height.times.map{'empty'})}
+  end
+
+  def place_piece(horizontal_coord, color)
+    color = color == 'black' ? "\u25CE" : "\u25C9"
+
+    return false if horizontal_coord >= @width
+    next_empty = pick_first_empty_slot(@grid[horizontal_coord])
+    if next_empty 
+      @grid[horizontal_coord][next_empty] = color
+    else
+      return false
+    end
+  end
+
+  def pick_first_empty_slot(column)
+    column.each_with_index do |slot, index|
+      return index if slot == 'empty'
+    end
+    return false
+  end
+
+  def game_over?
+
+    #Check to see if there are any vertical matches
+    vertical_count = 1
+
+    @grid.each_with_index do |column, column_index|
+      column.each_with_index do |slot, slot_index|
+
+        if slot_index != 0 && slot == column[slot_index - 1] && slot != 'empty'
+          vertical_count += 1
+          return true if vertical_count == 4
+        else 
+          vertical_count = 1
+        end
+
+        horizontal_count = 1
+
+        @grid.each_with_index do |column2, column2_index|
+          #Check to see if there are any horizontal matches
+
+          if column2_index != 0 && column[slot_index] == @grid[column2_index - 1][slot_index] && column2[slot_index] != 'empty'
+            horizontal_count += 1
+            return true if horizontal_count == 4
+          else
+            horizontal_count = 1
+          end
+
+          #Check to see if there are any diagonal matches in the right direction
+          if column2_index < @height - 4 && slot_index < @width - 4
+            return true if @grid[column2_index][slot_index] != 'empty' &&
+                           @grid[column2_index][slot_index] == @grid[column2_index+1][slot_index+1] &&
+                           @grid[column2_index+1][slot_index+1] == @grid[column2_index+2][slot_index+2] &&
+                           @grid[column2_index+2][slot_index+2] == @grid[column2_index+3][slot_index+3] 
+          end
+          #Check to see if there are any diagonal matches in the left direction
+          if column2_index < @height - 4 && slot_index > 2
+            return true if @grid[column2_index][slot_index] != 'empty' &&
+                           @grid[column2_index][slot_index] == @grid[column2_index+1][slot_index+1] &&
+                           @grid[column2_index+1][slot_index+1] == @grid[column2_index+2][slot_index+2] &&
+                           @grid[column2_index+2][slot_index+2] == @grid[column2_index+3][slot_index+3]
+          end
+        end
+      end
+    end
+    return false
+  end
+
+  def print_board
+    vertical_coord = @height-1
+    while vertical_coord >= 0 
+      print_array = ''
+      horizontal_coord = 0
+      while horizontal_coord < @width - 1
+        print_array += @grid[horizontal_coord][vertical_coord].encode('utf-8').center(7) + '|' 
+        horizontal_coord += 1
+      end
+      puts print_array
+      puts '-'*@width*7
+      vertical_coord -= 1
+    end
+  end
 end
+
+class Players
+  attr_reader :black_player, :red_player
+
+  def initialize(black, red)
+    @black_player = black
+    @red_player = red
+  end
+end
+
+class Turns
+  attr_reader :turn_count
+  def initialize(players)
+    @turn_count = 0
+    @black_player = players.black_player
+    @red_player = players.red_player
+  end
+
+  def increment
+    @turn_count += 1
+  end
+
+  def return_player
+    return @turn_count%2 == 0 ? @red_player : @black_player
+  end
+
+  def return_color
+    return @turn_count%2 == 0 ? 'red' : 'black'
+  end
+end
+
+def run
+  game_continue = true
+
+  puts 'What is the name of the player who will use black pieces?'
+  black = gets.chomp!
+  puts 'What is the name of the player who will use red pieces?'
+  red = gets.chomp!
+
+  game = Board.new
+  players = Players.new(black, red)
+  turns = Turns.new(players)
+
+  while game_continue do
+
+    puts "The current board looks like:"
+    print "#{game.print_board}"
+    valid_response = false
+    until valid_response do
+      puts "It is #{turns.return_player}'s turn. Please pick a slot from 1 to 7 to place your next piece in:"
+      position = gets.chomp!
+      if position.to_i.to_s == position
+        position = position.to_i - 1
+        valid_response = true if position >= 0 && position < 7 && game.place_piece(position, turns.return_color)
+      end
+    end
+
+    turns.increment
+
+    valid_response = false
+    if game.game_over? 
+      puts "#{turns.return_player} has gotten four in a row! Congratulations. Play again?"
+      response = gets.chomp!.downcase
+      valid_resopnse = true if response == 'yes' || response == 'no'
+      game_continue = false if response == 'no'
+      game = Board.new if response == 'yes'
+    end
+  end
+end
+
+run
--- rubymisc/connect_four/spec/connect_four_spec.rb
+++ rubymisc/connect_four/spec/connect_four_spec.rb
@@ -1,23 +1,124 @@
 require "connect_four"
 
-include ConnectFour
+describe 'board' do
 
-describe Player do
-	
-end
-	
-describe Board do
+	before :each do
+		@game = Board.new
+	end
 
-end
+	it 'should be empty at start' do
+		expect(@game.grid).to eq [['empty']*6]*7
+	end
 
-context "when player 1 has won" do
-	it "returns true for player 1 as the winner and false for player 2"
+	describe 'piece behavior' do
+		context 'when the piece is the first in the column' do
+			it 'should be in the first slot of the correct column' do
+				@game.place_piece(0, 'red')
+				expect(@game.grid).to eq [["\u25C9"]+['empty']*5]+[['empty']*6]*6
+			end
+		end
+
+		context 'when the piece is not the first piece in the column' do
+			it 'should be above an existing piece in the column' do
+				@game.place_piece(0, 'red')
+				@game.place_piece(0, 'black')
+				expect(@game.grid).to eq [["\u25C9", "\u25CE"]+['empty']*4]+[['empty']*6]*6
+			end
+		end
+
+		context 'when there are already the max number of pieces in a column' do
+			it 'should be unable to be placed in the column' do
+				6.times { @game.place_piece(0, 'red') }
+				expect(@game.place_piece(0, 'red')).to be false
+			end
+		end
+	end
+
+	describe 'game status behavior' do 
+		
+		context 'when there are only three pieces in a row' do
+			it 'should not be game over' do
+				3.times { @game.place_piece(0, 'black') }
+				expect(@game.game_over?).to be false
+			end
+		end
+
+		context 'when there are four pieces in a row vertically' do
+			it 'should be game over if they are the same color' do
+				4.times { @game.place_piece(3, 'black') }
+				expect(@game.game_over?).to be true
+			end
+
+			it 'shouldn\'t be game over if they are not the same color' do
+				3.times { @game.place_piece(3, 'black') }
+				@game.place_piece(3, 'red')
+				expect(@game.game_over?).to be false
+			end
+		end
+
+		context 'when there are four pieces in a row horizontally' do
+			it 'should be game over if they are the same color' do
+				@game.place_piece(0, 'red')
+				@game.place_piece(1, 'red')
+				@game.place_piece(2, 'red')
+				@game.place_piece(3, 'red')
+				expect(@game.game_over?).to be true
+			end
+
+			it 'shouldn\'t be game over if they are not the same color' do
+				@game.place_piece(0, 'red')
+				@game.place_piece(1, 'black')
+				@game.place_piece(2, 'red')
+				@game.place_piece(3, 'black')
+				expect(@game.game_over?).to be false
+			end
+		end
+
+		context 'when there are four pieces in a row diagonally' do
+			it 'matches in a right diagonal pattern' do 
+				@game.place_piece(0, 'red')
+				@game.place_piece(1, 'black')
+				@game.place_piece(1, 'red')
+				@game.place_piece(2, 'black')
+				@game.place_piece(2, 'red')
+				@game.place_piece(2, 'red')
+				@game.place_piece(3, 'red')
+				@game.place_piece(3, 'black')
+				@game.place_piece(3, 'black')
+				@game.place_piece(3, 'red')
+
+				expect(@game.game_over?).to be true
+			end
+
+			it 'matches in a left diagonal direction' do
+				@game.place_piece(6, 'red')
+				@game.place_piece(5, 'black')
+				@game.place_piece(5, 'red')
+				@game.place_piece(4, 'black')
+				@game.place_piece(4, 'red')
+				@game.place_piece(4, 'red')
+				@game.place_piece(3, 'red')
+				@game.place_piece(3, 'black')
+				@game.place_piece(3, 'black')
+				@game.place_piece(3, 'red')
+			end
+		end
 	end
 end
 
-context "when player 2 has won" do
-	it "returns true for player 2 as the winner and false for player 1"
+describe 'players' do
+	
+	before :all do
+		@players = Players.new('Black Player', 'Red Player')
+		@turns = Turns.new(@players)
 	end
-end
 
+	it 'should return the red player name on the even turns' do
+		expect(@turns.return_player).to eq 'Red Player'
+	end
+
+	it 'should return the black player name on odd turns' do
+		@turns.increment
+		expect(@turns.return_player).to eq 'Black Player'
+	end
 end
