0cdd052a0e9060d5b18b2a9c6772cb24069be890
Deep Red <0k5786@gmail.com>
2016-11-15 20:13:35 -0500

    I3: web_guesser number guessing game complete

--- Rails/web_guesser/web_guesser.rb
+++ Rails/web_guesser/web_guesser.rb
@@ -1,8 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-x = rand(100)
+SECRET_NUMBER = rand(100)
+
+def check_guess(g)
+  diff = (g.to_i - SECRET_NUMBER)
+  case
+  when diff > 5
+    "Way too high!"
+  when 5 > diff && diff > 0
+    "Too high!"
+  when -5 < diff && diff < 0
+    "Too low!"
+  when diff < -5
+    "Way too low!"
+  when diff == 0
+    "You got it right!"
+  else
+    "Guess not recognized."
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:number => x}
+  g = params['guess']
+  message = check_guess(g)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
