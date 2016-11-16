fae56549716ff863f73fff55b1b9d60b7f284541
Deep Red <0k5786@gmail.com>
2016-11-15 20:57:57 -0500

    web_guesser completed with extensions.

--- Rails/web_guesser/web_guesser.rb
+++ Rails/web_guesser/web_guesser.rb
@@ -1,28 +1,50 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
-
+@@secret_number = rand(100)
+@@attempts_remaining = 5
+@bg_color = "white"
 def check_guess(g)
-  diff = (g.to_i - SECRET_NUMBER)
+  diff = (g.to_i - @@secret_number)
   case
   when diff > 5
+    @bg_color = "#FF0000"
     "Way too high!"
-  when 5 > diff && diff > 0
+  when 5 >= diff && diff > 0
+    @bg_color = "#FFAAAA"
     "Too high!"
-  when -5 < diff && diff < 0
+  when -5 <= diff && diff < 0
+    @bg_color = "#FFAAAA"
     "Too low!"
   when diff < -5
+    @bg_color = "#FF0000"
     "Way too low!"
   when diff == 0
+    @bg_color = "#00FF00"
     "You got it right!"
   else
+    @bg_color = "#333333"
     "Guess not recognized."
+  end
+end
+
+def game_status
+  if @message == "You got it right!"
+    @@secret_number = rand(100)
+    @message = "You win! A new number has been generated!"
+  elsif @@attempts_remaining <= 0
+    @@secret_number = rand(100)
+    @@attempts_remaining = 5
+    @message = "You lost! Try to guess the new number!"
+  else
+    @@attempts_remaining -= 1
   end
 end
 
 get '/' do
   g = params['guess']
-  message = check_guess(g)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  cheat_mode = params['cheat']
+  @message = check_guess(g)
+  game_status
+  erb :index, :locals => {:number => @@secret_number, :message => @message, :bgc => @bg_color, :cheat_mode => cheat_mode}
 end
