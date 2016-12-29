d4efeb0d32ef13a07c201b7618e61a527e636a5b
Deep Red <0k5786@gmail.com>
2016-12-29 01:14:10 -0500

    Form Project 1 complete...ish step 4 under 'Editing' says that edits that fail validations should be putting a border on the fields with errors. That doesn't seem to be happening, just the submit button goes dead. Everything works on a proper edit though.

--- Rails/re-former/app/controllers/users_controller.rb
+++ Rails/re-former/app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-#    @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+#    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
@@ -12,6 +13,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user_params)
+  end
+  
   private
   def user_params
     params.require(:user).permit(:username,:email,:password)
