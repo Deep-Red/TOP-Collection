fc8a40ceb70e2568a77955564c9a9cdf1f223f56
Deep Red <0k5786@gmail.com>
2017-01-19 19:51:04 -0500

    railstutorial Rails/sample_app completed: Add user following.

--- Rails/sample_app/config/routes.rb
+++ Rails/sample_app/config/routes.rb
@@ -5,13 +5,17 @@ Rails.application.routes.draw do
   get '/contact',   to: 'static_pages#contact'
   get '/signup',    to: 'users#new'
   post '/signup',   to: 'users#create'
-  get 'users/new'
   get '/login',     to: 'sessions#new'
   post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :relationships,       only: [:create, :destroy]
 end
