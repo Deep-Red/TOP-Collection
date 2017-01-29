81f9726cc6c672ddc6c511ed8d05de2c3ad4523a
Deep Red <0k5786@gmail.com>
2017-01-29 01:22:39 -0500

    More work on the odin-flight-booker. The new booking view renders a form that adjusts the number of fields to the number of passengers. Currently it only submits and creates the last one though.

--- Rails/odin-flight-booker/app/controllers/bookings_controller.rb
+++ Rails/odin-flight-booker/app/controllers/bookings_controller.rb
@@ -2,14 +2,26 @@ class BookingsController < ApplicationController
   def new
     @flight_number ||= params[:flight]
     @flight ||= Flight.find_by_id(params[:flight])
-    @passengers = params[:passenger_count]
 
     @booking ||= @flight.bookings.build
-    @num_passengers = params[:passengers].to_i
+    @num_passengers = params[:passenger_count].to_i
 
+    @passengers = []
+    @num_passengers.times do |i|
+      @passengers << Passenger.new
+    end
   end
 
   def create
+    if params.has_key?("passenger")
+      Passenger.create(passenger_params(params[:passenger]))
+    else
+      params["passengers"].each do |passenger|
+        if passenger["name"] != "" || passenger["email"] != ""
+          Passenger.create(name: params[:passengers][:name], email: params[:passengers][:email])
+        end
+      end
+    end
     @booking = Booking.new
   end
 
@@ -19,6 +31,7 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    require.permit(:flight, :passenger_count)
+    require.permit(:flight, :passenger_count, :passengers => [:name, :email])
   end
+
 end
