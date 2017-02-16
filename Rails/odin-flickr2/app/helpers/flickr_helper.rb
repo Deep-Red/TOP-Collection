8a6e42624abc247c9ebf2a14fd4a8d76e128496f
Deep Red <0k5786@gmail.com>
2017-02-16 04:39:04 -0500

    odin-flickr2 displays images from flickr as links to a larger version. Needs organization and a way to change which user they are displayed for.

---
+++ Rails/odin-flickr2/app/helpers/flickr_helper.rb
@@ -0,0 +1,23 @@
+module FlickrHelper
+  def user_photos(user_id, photo_count = 12)
+
+    FlickRaw.api_key = ENV['API_KEY']
+    FlickRaw.shared_secret = ENV['SHARED_SECRET']
+
+    flickr = FlickRaw::Flickr.new
+
+    return flickr.photos.search(:user_id => user_id, :photo_count => photo_count)
+
+  end
+
+  def render_flickr_sidebar_widget(user_id, photo_count = 12, columns = 2)
+    begin
+      photos = user_photos(user_id, photo_count)
+      puts "BBBBBBBBBBBBBBBBBB"
+      puts photos.count
+      render :partial => '/flickr/sidebar_widget', :locals => { :photos => photos }
+    rescue Exception
+      render :partial => '/flickr/unavailable'
+    end
+  end
+end
