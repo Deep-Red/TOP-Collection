7e01f33158df8ab72513017d57e616e8abdaf452
Deep Red <0k5786@gmail.com>
2017-04-19 03:17:56 -0400

    Manipulating the DOM project through step 5.

---
+++ js_and_jq/jQuery-restaurant/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('#content').append('<h1>The Restaurant at the end of Texas</h1>');
+  $('#content').append('<img alt="Flag of Texas" src="http://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Flag_of_Texas.svg/125px-Flag_of_Texas.svg.png">');
+  $('#content').append('<p>At the restaurant you can meet and dine with (mayan meetan con with dinan on when) a fascinating cross-section of the entire population of space and time.</p>');
+
+});
