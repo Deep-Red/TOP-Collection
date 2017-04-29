91dc564a69573ce8be71bd053b485dac417f490f
Deep Red <0k5786@gmail.com>
2017-04-28 20:13:07 -0400

    JavaScript added. Images 'flip' when 'next' button is pressed... most of the time.

---
+++ js_and_jq/image_carousel/carousel.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+var slideout = function() {
+
+}
+
+var next = function() {
+
+  var oldslide =  $('.previous');
+  var currslide = $('.active');
+  oldslide.removeClass('previous');
+  currslide.addClass('previous');
+  currslide.removeClass('active');
+  if (currslide.hasClass('last')) {
+    var newslide = currslide.siblings().filter(':first');
+  } else {
+    var newslide = currslide.next();
+    currslide.animate({"left": "toggle" }, 600);
+  };
+  newslide.addClass('active');
+
+};
+
+$('#next-btn').click(function() {
+  console.log("AA");
+  next();
+});
+
+});
