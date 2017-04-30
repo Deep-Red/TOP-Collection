c843dd18a7f79d3cd524dba519ace0f5d1e62dd6
Deep Red <0k5786@gmail.com>
2017-04-30 01:30:26 -0400

    Manually cyling forward and backward functional.

--- js_and_jq/image_carousel/carousel.js
+++ js_and_jq/image_carousel/carousel.js
@@ -6,29 +6,61 @@ var slideout = function() {
 
 var next = function() {
 
-  var allslides = $('.slides');
-  var oldslide =  $('.previous');
-  var currslide = $('.active');
-  var cswidth = currslide.width();
+  var $allslides = $('.slides');
+  var $oldslide =  $('.previous');
+  var $currslide = $('.active');
+  var cswidth = $currslide.width();
 
-  oldslide.removeClass('previous');
-  currslide.addClass('previous');
-  currslide.removeClass('active');
-  if (currslide.next().hasClass('last')) {
-    allslides.animate({"margin-left": "-=" + cswidth}, function() {
+  $oldslide.removeClass('previous');
+  $currslide.addClass('previous');
+  $currslide.removeClass('active');
+
+  if ($currslide.next().hasClass('last')) {
+    $allslides.animate({"margin-left": "-=" + cswidth}, 1200, function() {
       $(this).css('margin-left', 0)});
-    var newslide = currslide.siblings().filter(':first');
+    var newslide = $currslide.siblings().filter(':first');
   } else {
-    allslides.animate({"margin-left": "-=" + cswidth});
-    var newslide = currslide.next();
+    $allslides.animate({"margin-left": "-=" + cswidth}, 1200);
+    var newslide = $currslide.next();
   };
+
   newslide.addClass('active');
 
 };
 
+var prev = function() {
+
+  var $allslides = $('.slides');
+  var $oldslide =  $('.previous');
+  var $currslide = $('.active');
+  var cswidth = $currslide.width();
+  var slidescount = $('.slide').length;
+
+  $oldslide.removeClass('previous');
+  $currslide.addClass('previous');
+  $currslide.removeClass('active');
+
+  if ($currslide.prev().hasClass('first')) {
+    $allslides.animate({"margin-left": "+=" + cswidth}, 1200, function() {
+      $(this).css('margin-left', (-cswidth * (slidescount-1)))});
+    var newslide = $currslide.siblings().filter(':last');
+  } else {
+    $allslides.animate({"margin-left": "+=" + cswidth}, 1200);
+    var newslide = $currslide.prev();
+  };
+
+  newslide.addClass('active');
+
+}
+
 $('#next-btn').click(function() {
-  console.log("AA");
+  console.log("Next Slide");
   next();
 });
+
+$('#prev-btn').click(function() {
+  console.log("Previous Slide");
+  prev();
+})
 
 });
