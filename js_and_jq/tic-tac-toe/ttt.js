9b23da347f550920013ce9c8537e97384bf86364
Deep Red <0k5786@gmail.com>
2017-05-04 02:03:30 -0400

    TTT board laid out. Xs and Os can be placed - only in unoccupied squares.

--- js_and_jq/tic-tac-toe/ttt.js
+++ js_and_jq/tic-tac-toe/ttt.js
@@ -1,9 +1,34 @@
-var board = {
-  row1 = [" ", " ", " "];
-  row2 = [" ", " ", " "];
-  row3 = [" ", " ", " "];
+$(document).ready(function() {
 
-  render:function() {
-    
+var board = function() {
+  $("#boardbox").append("<div class='grid'></div>");
+  for (var i = 0; i < 3; ++i){
+    $(".grid").append("<div class='row' id='row-"+i+"'></div>");
+  }
+  for (var j = 0; j < 3; ++j){
+    $(".row").append("<div class='square square-"+j+" empty-square'></div>");
   }
 }
+
+
+var turns = 0;
+board();
+
+function makeMove() {
+  if ($(this).hasClass('empty-square'))
+  {
+    turns++;
+    if (turns % 2) {
+      $(this).removeClass("empty-square") && $(this).append("<p>X</p>");
+    } else {
+      $(this).removeClass("empty-square") && $(this).append("<p>O</p>");
+    }
+  }
+};
+
+
+$(".square").on('click', function() {
+  makeMove.call(event.target);
+});
+
+});
