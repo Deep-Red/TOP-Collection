c96a2feb0759fdab73dd9616c5eb8b284f45cec9
Deep Red <0k5786@gmail.com>
2017-05-08 21:36:43 -0400

    Basic play working. Recursive reveal buggy, but not unplayable. Input for changing board size not working. Needs timer and counter.

--- js_and_jq/minesweeper/minesweeper.js
+++ js_and_jq/minesweeper/minesweeper.js
@@ -14,7 +14,7 @@ var Board = function(rows, cols, mines){
   for (var i = 0; i < rows; i++) {
     $('#minefield').append("<div class='row' id='row-"+i+"'></div>");
     cells.push([]);
-    console.log("i" + cells);
+//    console.log("i" + cells);
   }
   for (var j = 0; j < rows; j++) {
     for(var k = 0; k < cols; k++) {
@@ -22,7 +22,7 @@ var Board = function(rows, cols, mines){
       cells[j].push(new Cell(false, false, j, k));
     }
   }
-  console.log(cells);
+//  console.log(cells);
 }
 
 var setMines = function(count) {
@@ -30,33 +30,121 @@ var setMines = function(count) {
     r = Math.floor(Math.random() * height);
     c = Math.floor(Math.random() * width);
     cells[r][c].mined = true;
-    console.log(r+" "+c);
+//    console.log(r+""+c);
+
+    $('#cell-'+r+""+c).addClass('mined');
+    if (c < 8) {
+      cells[r][(c+1)].touching++;
+//      $('#cell-'+(r)+""+(c+1)).addClass('flagged');
+
+      if (r < 8) {
+        cells[(r+1)][(c+1)].touching++;
+//        $('#cell-'+(r+1)+""+(c+1)).addClass('flagged');
+
+      }
+      if (r > 0) {
+        cells[(r-1)][(c+1)].touching++;
+//        $('#cell-'+(r-1)+""+(c+1)).addClass('flagged');
+
+      }
+    }
+    if (c > 0) {
+      cells[r][(c-1)].touching++;
+//      $('#cell-'+(r)+""+(c-1)).addClass('flagged');
+
+      if (r < 8) {
+        cells[r+1][(c-1)].touching++;
+//        $('#cell-'+(r+1)+""+(c-1)).addClass('flagged');
+
+      }
+      if (r > 0) {
+        cells[(r-1)][(c-1)].touching++;
+//        $('#cell-'+(r-1)+""+(c-1)).addClass('flagged');
+
+      }
+    }
+    if (r < 8) {
+      cells[r+1][(c)].touching++;
+//      $('#cell-'+(r+1)+""+(c)).addClass('flagged');
+
+    }
+    if (r > 0) {
+      cells[(r-1)][c].touching++;
+//      $('#cell-'+(r-1)+""+(c)).addClass('flagged');
+
+    }
+
   }
 }
-//Board.prototype.render = function(){
-//  setMines: function() {
-//
-//  },
 
-//};
-
-
-var height = 9;
-var width = 9;
-var mines = 9;
-var board = new Board(height, width, mines);
-setMines(mines);
-
-$('.cell').on('click', function() {
-  console.log(event.target.id + event.which);
-});
-$('.cell').on('contextmenu', function(){
-  event.preventDefault();
-  console.log(event.target.id + event.which);
-  $(this).toggleClass('flagged');
+var toggleFlag = function () {
   r = event.target.id.split("")[5];
   c = event.target.id.split("")[6];
   cells[r][c].flagged = !cells[r][c].flagged;
+  $(this).toggleClass('flagged');
+}
+var boom = function () {
+  console.log("Boom");
+  $('.mined').addClass('exploded');
+}
+
+var height = $('#rowinput').value || 9;
+var width = $('#colinput').value || 9;
+var mines = $('#mininput').value || 9;
+var board = new Board(height, width, mines);
+setMines(mines);
+
+console.log($('#rowinput'));
+console.log($('#rowinput').value);
+console.log($('#rowinput')[0]);
+console.log(height);
+console.log($('form').serialize());
+var formData = new FormData(document.querySelector('form'));
+console.log(formData);
+var formnumber = document.getElementsByName("rows").value;
+console.log(formnumber);
+
+
+$('.cell').on('click', function() {
+  if (!$(this).hasClass('clicked')) {
+    $(this).addClass('clicked');
+//    console.log("Clicked on "+$(this)[0].id);
+    var r = Number($(this)[0].id.split("")[5]);
+    var c = Number($(this)[0].id.split("")[6]);
+//    console.log($(this)[0].id + event.which);
+    if (cells[r][c].mined) {
+      boom.call($(this));
+    } else if ((cells[r][c].touching) > 0) {
+      $(this).append("<p>"+cells[r][c].touching+"</p>");
+    } else {
+      for (i = -1; i < 2; i++) {
+        for (j = -1; j < 2; j++) {
+          $('#cell-'+(r+i)+""+(c+j)).click();
+        }
+      }
+//      console.log('#cell-'+r+''+(c+1));
+//      $('#cell-'+r+""+(c+1)[0]).click();
+//      console.log("AAAAAAAAAAAA");
+//      $('#cell-'+r+""+(c-1)[0]).click();
+//      $('#cell-'+(r+1)+""+c[0]).click();
+//      $('#cell-'+(r+1)+""+(c-1)[0]).click();
+//      console.log("BBBBBBBBBBBB");
+//      $('#cell-'+(r+1)+""+(c+1)[0]).click();
+//      $('#cell-'+(r-1)+""+c[0]).click();
+//      $('#cell-'+(r-1)+""+(c-1)[0]).click();0
+//      console.log($('#cell-'+(r-1)+""+(c+1)[0]).click());
+//      console.log("CCCCCCCCCCCC");
+    }
+  }
+});
+$('.cell').on('contextmenu', function(){
+  event.preventDefault();
+  toggleFlag.call($(this));
+//  console.log(($('.mined').not('.flagged').length));
+//  console.log(($('.flagged').not('.mined').length));
+  if (($('.mined').not('.flagged').length === 0) && ($('.flagged').not('.mined').length === 0)) {
+    console.log("YOU WIN");
+  }
 })
 
 });
