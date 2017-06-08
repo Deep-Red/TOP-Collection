1a08e7b556c3abdf5cc3b08eb538c0fb3c8d4f01
Deep Red <0k5786@gmail.com>
2017-06-08 16:39:25 -0400

    Game works. High score model implemented, but displaying a popup form in addition to the flash is turning out to be problematic.

--- js_and_jq/waldo/app/controllers/application_controller.rb
+++ js_and_jq/waldo/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  respond_to :js, :html, :xml
+
+  def respond_modal_with(*args, &blk)
+    options = args.extract_options!
+    options[:responder] = ModalResponder
+    respond_with(*args, options, &blk)
+  end
 end
--- js_and_jq/waldo/app/controllers/static_pages_controller.rb
+++ js_and_jq/waldo/app/controllers/static_pages_controller.rb
@@ -1,4 +1,58 @@
 class StaticPagesController < ApplicationController
   def home
+    @@counter = 0
+    @@start = Time.now
+  end
+
+  def checkItem
+    answer = checkAnswer(params[:x], params[:y], params[:item]);
+    if (@@counter == 3)
+      @@official_score = score
+      flash[:success] = "Nice! You found all three in #{@@official_score} seconds"
+      render js: "location.reload()"
+    else
+      render json: answer
+    end
+  end
+
+  def enter_score
+    if highscore?
+      @newscore = Score.new
+      @newscore.score = @@official_score
+      respond_modal_with @newscore
+    end
+  end
+
+  def record_score(score)
+    render
+    s = Score.new
+    s.score = score
+    s.name = "A"
+    s.save
+  end
+
+  private
+
+  def score
+    ( Time.now - @@start ).floor
+  end
+
+  def highscore?
+    low_high = Score.order('score').first
+    low_high.score > @@official_score ? true : false
+  end
+
+  def display_scores
+    @scores = Score.all.order('score asc')
+  end
+
+  def checkAnswer (x, y, name)
+    c = Item.where(name: name.to_s)[0]
+    if (x.to_i.between?(c.xpos - 15, c.xpos + 15) && y.to_i.between?(c.ypos - 15, c.ypos + 15))
+      @@counter += 1
+      return {name: c.name, xpos: c.xpos, ypos: c.ypos}
+    else
+      return false
+    end
   end
 end
